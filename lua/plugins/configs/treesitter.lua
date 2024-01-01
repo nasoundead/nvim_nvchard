660159cc4e4e904de1c7a9bae0ac6fcda5237e92
nasoundead <nasoundead@163.com>
2024-01-01 12:55:16 +0800

    treesitter.lua

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,20 +1,47 @@
 local options = {
-    ensure_installed = { -- c
-    "c", "cmake", "cpp", -- language
-    "python", "rust", "java", "sql", "go", "gomod", "gosum", -- script
-    "lua", "fish", "dockerfile", "git_config", "bash", "awk", "vim", -- web
-    "css", "typescript", "html", "json", "json5", "javascript", "http", "tsx", -- config
-    "diff", "xml", "yaml", "toml", "comment", -- notes
-    "markdown", "org"},
+	ensure_installed = { -- c
+		"c",
+		"cmake",
+		"cpp", 
+		"python",
+		"rust",
+		"java",
+		"sql",
+		"go",
+		"gomod",
+		"gosum",
+		"lua",
+		"fish",
+		"dockerfile",
+		"git_config",
+		"bash",
+		"awk",
+		"vim", 
+		"css",
+		"typescript",
+		"html",
+		"json",
+		"json5",
+		"javascript",
+		"http",
+		"tsx",
+		"diff",
+		"xml",
+		"yaml",
+		"toml",
+		"comment",
+		"markdown",
+		"org",
+	},
 
-    highlight = {
-        enable = true,
-        use_languagetree = true
-    },
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
 
-    indent = {
-        enable = true
-    }
+	indent = {
+		enable = true,
+	},
 }
 
 return options
