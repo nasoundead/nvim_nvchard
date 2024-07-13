b69bdd9d9b1b88b988e2e1d87c21e394b468f8fb
nasoundead <nasoundead@163.com>
2024-07-13 22:00:29 +0800

    FiraCode

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -68,7 +68,9 @@ opt.number = true
 opt.numberwidth = 4
 opt.ruler = false
 
-opt.guifont = "JetBrainsMono Nerd Font:h11"
+-- opt.guifont = "Iosevka Nerd Font:h11"
+opt.guifont = "FiraCode Nerd Font:h12"
+-- opt.guifont = "JetBrainsMono Nerd Font:h11"
 -- opt.guifont = "FiraCode Nerd Font:h11"
 
 -- disable nvim intro
