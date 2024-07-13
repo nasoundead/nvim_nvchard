558e639ffe397956ade2edce67072622e4572c34
nasoundead <nasoundead@163.com>
2024-07-13 22:52:56 +0800

    multi cursors

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -282,7 +282,7 @@ M.nvimtree = {
 
     n = {
         -- toggle
-        ["<C-n>"] = {"<cmd> NvimTreeToggle <CR>", "Toggle nvimtree"},
+        ["<C-e>"] = {"<cmd> NvimTreeToggle <CR>", "Toggle nvimtree"},
 
         -- focus
         ["<leader>e"] = {"<cmd> NvimTreeFocus <CR>", "Focus nvimtree"}
